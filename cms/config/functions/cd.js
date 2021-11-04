fc10219e8b57eeb27c81a037e3217f8778f0e452
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-04 23:06:30 +0000

    remove axios

--- cms/config/functions/cd.js
+++ cms/config/functions/cd.js
@@ -1,26 +1,4 @@
 'use strict';
 
-const axios = require('axios');
-
 module.exports = async () => {
-    const host = "https://api.github.com";
-    const owner = "Level8Broccoli";
-    const repo = "eagleslinedancers.ch";
-    const url = `${host}/repos/${owner}/${repo}/dispatches`;
-    const eventType = "fetch";
-
-    try {
-        await axios({
-            method: "post",
-            url,
-            data: { "event_type": eventType },
-            headers: { "Accept": "application/vnd.github.v3+json", "Content-Type": "application/json" },
-            auth: {
-                username: process.env.GH_USERNAME,
-                password: process.env.GH_PASSWORD,
-            }
-        });
-    } catch (error) {
-        console.log({ error });
-    }
 };
