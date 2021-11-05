66ee98c88b9943793a69ba51895ca17c953a239b
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-05 18:33:53 +0000

    update cd shell script

--- cms/cd.sh
+++ cms/cd.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 
-if [ -d /tmp/eagleslinedancers.ch ]; then
-  rm -R /tmp/eagleslinedancers.ch
-fi
-mkdir -p /tmp/eagleslinedancers.ch;
+cd /tmp
 
-GIT_SSH_COMMAND='ssh -i /tmp/.ssh/id_rsa' git clone git@github.com:Level8Broccoli/eagleslinedancers.ch.git /tmp/eagleslinedancers.ch
-echo "after clone"
-ls /tmp/eagleslinedancers.ch
-echo "before curl"
+if [ -d eagleslinedancers.ch ]; then
+  rm -R eagleslinedancers.ch;
+fi
+
+GIT_SSH_COMMAND='ssh -i /tmp/.ssh/id_rsa' git clone git@github.com:Level8Broccoli/eagleslinedancers.ch.git
+
+cd eagleslinedancers.ch
+
 curl \
   -X GET \
   -H "Content-Type: application/json" \
-  -o /tmp/eagleslinedancers.ch/data-seiten.json \
+  -o data-seiten.json \
   http://localhost:1337/seiten
-echo "after curl"
-ls /tmp/eagleslinedancers.ch
-echo "before push"
-git diff --quiet && git diff --staged --quiet || git commit -am 'fetch new seiten data' && GIT_SSH_COMMAND='ssh -i /tmp/.ssh/id_rsa' git push
-echo "after push"
 
-rm -R /tmp/eagleslinedancers.ch
+
+if [ ! $(git diff --quiet) ] || [ ! $(git diff --staged --quiet) ]; then
+  git commit -am 'fetch new seiten data';
+  GIT_SSH_COMMAND='ssh -i /tmp/.ssh/id_rsa' git push;
+fi
