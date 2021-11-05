d10cb042dc2fa667a2adaa445521455a1259342e
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-05 17:53:49 +0000

    fetch new seiten data

--- cms/cd.sh
+++ cms/cd.sh
@@ -7,12 +7,16 @@ if [ ! -d /tmp/eagleslinedancers.ch ]; then
 fi
 
 GIT_SSH_COMMAND='ssh -i /tmp/.ssh/id_rsa' git clone git@github.com:Level8Broccoli/eagleslinedancers.ch.git /tmp/eagleslinedancers.ch
+echo "after clone"
 ls /tmp/eagleslinedancers.ch
+echo "before curl"
 curl \
   -X GET \
   -H "Content-Type: application/json" \
   -o /tmp/eagleslinedancers.ch/data-seiten.json \
   http://localhost:1337/seiten
+echo "after curl"
 git diff --quiet && git diff --staged --quiet || git commit -am 'fetch new seiten data' && git push
+echo "after push"
 
 rm -R /tmp/eagleslinedancers.ch
