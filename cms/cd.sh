6424054b4be56c26f417b9edf0cff92fe614b6ba
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-06 23:41:16 +0000

    Remove StrictHostKeyChecking=no
    use known_hosts instead

--- cms/cd.sh
+++ cms/cd.sh
@@ -2,7 +2,6 @@
 
 TEMP_FOLDER="/tmp"
 GIT_NAME="eagleslinedancers.ch"
-SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i ${TEMP_FOLDER}/.ssh/id_rsa"
 TYPES="seiten startseite"
 
 cd $TEMP_FOLDER
@@ -12,7 +11,7 @@ then
   rm -R $GIT_NAME
 fi
 
-GIT_SSH_COMMAND="${SSH_COMMAND}" git clone git@github.com:Level8Broccoli/${GIT_NAME}.git
+git clone git@github.com:Level8Broccoli/${GIT_NAME}.git
 
 cd $GIT_NAME
 
@@ -31,5 +30,5 @@ then
   git config --global user.email "7040739+Level8Broccoli@users.noreply.github.com"
   git add .
   git commit -m 'sync data from cms'
-  GIT_SSH_COMMAND="${SSH_COMMAND}" git push
+  git push
 fi
