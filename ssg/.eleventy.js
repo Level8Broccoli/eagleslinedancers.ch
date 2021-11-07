a9663ce6c4f8d4370390feec2f81b1c54d118c98
GitHub Action <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-07 20:21:54 +0000

    add markdownify

--- ssg/.eleventy.js
+++ ssg/.eleventy.js
@@ -1,4 +1,12 @@
+const MarkdownIt = require('markdown-it');
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('styles/**/*.css')
     eleventyConfig.addPassthroughCopy({ 'public/': "/" })
+    eleventyConfig.addFilter('markdownify', (content) => {
+        const md = new MarkdownIt({
+            html: true
+        });
+        return md.render(content);
+    });
 }
