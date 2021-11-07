90fa1f03b5794e8f60573fa3fcdf171f669386b5
GitHub Action <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-07 22:48:17 +0000

    update further styles

--- ssg/.eleventy.js
+++ ssg/.eleventy.js
@@ -1,7 +1,7 @@
 const MarkdownIt = require('markdown-it');
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('styles/**/*.css')
+    eleventyConfig.addPassthroughCopy({ 'styles/css/': "/styles/" })
     eleventyConfig.addPassthroughCopy({ 'public/': "/" })
     eleventyConfig.addFilter('markdownify', (content) => {
         const md = new MarkdownIt({
