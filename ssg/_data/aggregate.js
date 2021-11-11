4abe08ba02cf1a10a0784562fbc32f56d4b73392
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-11 21:07:11 +0000

    setup pages and navigation

---
+++ ssg/_data/aggregate.js
@@ -0,0 +1,10 @@
+const fussnavigation = require("./fussnavigation.json");
+const hauptnavigation = require("./hauptnavigation.json");
+
+module.exports = () => {
+    const seitenData = [];
+
+    return {
+        seitenData,
+    }
+}
