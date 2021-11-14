ff8874926e610f855016800e15831aa2959160ea
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-11-14 16:45:46 +0000

    Add Navbar Skript

--- ssg/.eleventy.js
+++ ssg/.eleventy.js
@@ -2,6 +2,7 @@ const MarkdownIt = require('markdown-it');
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'styles/css/': "/styles/" })
+    eleventyConfig.addPassthroughCopy({ 'scripts/': "/scripts/" })
     eleventyConfig.addPassthroughCopy({ 'public/': "/" })
     eleventyConfig.addFilter('markdownify', (content) => {
         const md = new MarkdownIt({
