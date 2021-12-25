0d044d27b0317b8af1ad4989d64293f2ddb568e7
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-12-25 16:31:11 +0100

    new try

--- ssg/.eleventy.js
+++ ssg/.eleventy.js
@@ -4,6 +4,7 @@ const Image = require("@11ty/eleventy-img");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "styles/css/": "/styles/" });
   eleventyConfig.addPassthroughCopy({ "scripts/": "/scripts/" });
+  eleventyConfig.addPassthroughCopy({ ".img/": "/img" });
   eleventyConfig.addPassthroughCopy({ "public/": "/" });
   eleventyConfig.addFilter("markdownify", (content) => {
     const md = new MarkdownIt({
@@ -16,8 +17,8 @@ module.exports = function (eleventyConfig) {
     (src, alt, width, height, sizes = "100vw") => {
       let opts = {
         widths: [width],
-        formats: ["jpeg"],
-        outputDir: "./_site/img/",
+        formats: ["webp", "jpeg"],
+        outputDir: "./.img/",
       };
       Image(src, opts);
       let metadata = Image.statsByDimensionsSync(src, width, height, opts);
