bfae8e53e5508a2c7bff7a1c95d9125dcb32c32d
Oliver Bucher <lvl8io+gitlab@pm.me>
2023-11-26 19:45:00 +0000

    Astro: Improve images

--- astro-ssg/astro.config.mjs
+++ astro-ssg/astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    domains: [
+      "admin.eagleslinedancers.ch",
+      "eagleslinedancers.ch"
+    ],
+    remotePatterns: [
+      { protocol: "https" }
+    ],
+  }
+});
