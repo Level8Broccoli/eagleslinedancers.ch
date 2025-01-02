8bf22f07a4d391e5e3fa712185e6240be88a24c0
Level8Broccoli <7040739+Level8Broccoli@users.noreply.github.com>
2025-01-02 16:38:36 +0000

    Update manual podman helper

---
+++ cms/create-container.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This sadly does not contain the correct `/srv/app/cd.sh`. Please use the one in the git repo.
+
+podman run -d \
+  -p "3333:1337" \
+  -v "eagles-cms_db:/srv/app/.tmp" \
+  -v "eagles-cms_uploads:/srv/app/public/uploads" \
+  -v "eagles-cms_cd:/srv/app/.cd" \
+  docker.io/library/cms_eagles-cms
--- cms/start-container.sh
+++ cms/start-container.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-podman run -d -p "3333:1337" -v "eagles-cms_db:/srv/app/.tmp" -v "eagles-cms_uploads:/srv/app/public/uploads" docker.io/library/cms_eagles-cms 
+
+podman start ca3864c84c83
