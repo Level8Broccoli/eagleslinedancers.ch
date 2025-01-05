b3ec5b50dcea0d0b4ea5dd2c7b5c02197f7192a2
Level8Broccoli <7040739+Level8Broccoli@users.noreply.github.com>
2025-01-05 00:56:51 +0000

    Yarn Package: Draft

--- flake.nix
+++ flake.nix
@@ -2,25 +2,77 @@
   description = "eagleslinedancers.ch development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/404cd360c2607bcfe8d05a7d5ae609d6605f7e22"; # pin at yarn version 1.22.10
+    nixpkgs-yarn.url = "github:NixOS/nixpkgs/404cd360c2607bcfe8d05a7d5ae609d6605f7e22"; # pin at yarn version 1.22.10
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      nixpkgs-yarn,
+      # nixpkgs-unstable,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs-yarn { inherit system; };
       in
+      # pkgs = import nixpkgs-unstable { inherit system; };
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nodejs
             pkgs.yarn
           ];
+        };
+
+        packages = rec {
+          default = cms2;
+
+          cms = pkgs.mkYarnPackage {
+            src = ./cms;
+
+            nativeBuildInputs = [
+              pkgs.python39Packages.pyvips
+              pkgs.pkg-config
+            ];
+
+            buildInputs = [
+              pkgs.python39Packages.pyvips
+              pkgs.pkg-config
+            ];
+
+            buildPhase = ''
+              export HOME=$(mktemp -d)
+              export NODE_ENV=production
+              ${pkgs.yarn}/bin/yarn --offline build
+            '';
+
+            # installPhase = ''
+            # ${pkgs.yarn}/bin/yarn --offline start
+            # '';
+          };
+
+          cms2 = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "eagles-cms";
+            version = "0.0.1";
+
+            src = ./cms;
+
+            yarnOfflineCache = pkgs.fetchYarnDeps {
+              yarnLock = finalAttrs.src + "/yarn.lock";
+              hash = "sha256-Y/D2negye2IA40heckixEWl/eDw8k9f50nfRX6giXsY=";
+            };
+
+            nativeBuildInputs = with pkgs; [
+              yarnConfigHook
+              yarnBuildHook
+              yarnInstallHook
+              nodejs
+            ];
+          });
         };
       }
     );
