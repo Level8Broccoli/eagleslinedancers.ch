f30e55b0663960857817021ba49ab038e46c88ec
Oliver Bucher <oliver.bucher@getabstract.com>
2025-01-10 18:34:19 +0000

    Problems with glib-object.h

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs =
     {
-      nixpkgs,
+      # nixpkgs,
       nixpkgs-legacy,
       flake-utils,
       ...
@@ -17,14 +17,16 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        pkgs-legacy = import nixpkgs-legacy { inherit system; };
+        # pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs-legacy { inherit system; };
       in
+      # pkgs =pkgs;
+      #pkgs = pkgs;
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs-legacy.nodejs
-            pkgs-legacy.yarn
+            pkgs.nodejs
+            pkgs.yarn
           ];
         };
 
@@ -34,13 +36,76 @@
             alpine = pkgs.dockerTools.pullImage {
               imageName = "alpine";
               imageDigest = "sha256:56fa17d2a7e7f168a043a2712e63aed1f8543aeafdcee47c58dcffe38ed51099";
-              hash = "sha256-ZklI6Quz/DYp0JxNYuQTItmc9rp1Bob57frfk1BRpcA=";
-              finalImageName = "alpine";
-              finalImageTag = "3";
+              # hash = "sha256-ZklI6Quz/DYp0JxNYuQTItmc9rp1Bob57frfk1BRpcA=";
+              sha256 = "sha256-C3TOcLa18BKeBfS5FSe0H6BALGA/zXSwSZstK+VaPyo=";
             };
 
             cms = pkgs.mkYarnPackage {
-              src = ./cms;
+              src = builtins.path {
+                path = ./cms;
+                filter = path: type: !(type == "directory" && baseNameOf path == "node_modules");
+              };
+              extraBuildInputs = [
+                # pkgs.vips
+                # pkgs.vips.dev
+              ];
+
+              pkgConfig =
+                let
+                  nodeHeaders = pkgs.fetchurl {
+                    url = "https://nodejs.org/download/release/v${pkgs.nodejs.version}/node-v${pkgs.nodejs.version}-headers.tar.gz";
+                    sha256 = "sha256-JPpqOSUCeYDjLPolVdHMnsqYnbbAiQ/h4S4cn570uqc=";
+                  };
+                in
+                {
+                  sharp = {
+                    # nativeBuildInputs = builtins.attrValues {
+                    #   inherit (pkgs.nodePackages) node-gyp;
+                    #   inherit (pkgs) python3 pkg-config;
+                    # };
+                    buildInputs = [ pkgs.vips ];
+                    postInstall = ''
+                      export PYTHON=${pkgs.python3}/bin/python
+
+                      ${pkgs.nodePackages.node-gyp}/bin/node-gyp \
+                        rebuild --tarball="${nodeHeaders}"
+                    '';
+                  };
+                };
+              # pkgConfig = {
+              #   sharp =
+              #     let
+              #       nodeHeaders =pkgs.fetchurl {
+              #         name = "node-headers-${pkgs-legacy.nodejs.version}";
+              #         url = "https://nodejs.org/download/release/v${pkgs-legacy.nodejs.version}/node-v${pkgs-legacy.nodejs.version}-headers.tar.gz";
+              #         sha256 = "sha256-JPpqOSUCeYDjLPolVdHMnsqYnbbAiQ/h4S4cn570uqc=";
+              #         # sha256 = "sha256-DULcOzN39J5JWXbcDk9cOn/7HXFAUNLyR6/bvAiY2uU=";
+              #       };
+              #     in
+              #     {
+              #       nativeBuildInputs = withpkgs; [
+              #         pkgconfig
+              #         pkg-config
+              #         python3
+              #         nodePackages.node-gyp
+              #       ];
+              #       buildInputs = withpkgs; [ vips ];
+
+              #       # Add additional environment variables if needed
+              #       NIX_CFLAGS_COMPILE = [ "-I${pkgs-legacy.glib.dev}/include" ];
+              #       NIX_LDFLAGS = [ "-L${pkgs-legacy.glib.lib}" ];
+
+              #       # Set the PKG_CONFIG_PATH to include pkg-config files for glib during build
+              #       PKG_CONFIG_PATH = "${pkgs-legacy.glib.lib}/pkgconfig";
+
+              #       postInstall = ''
+              #         export PYTHON="${pkgs-legacy.python3}/bin/python"
+
+              #         ${pkgs-legacy.nodePackages.node-gyp}/bin/node-gyp rebuild --tarball="${nodeHeaders}"
+              #         ${pkgs-legacy.nodejs}/bin/node install/dll-copy
+              #       '';
+              #     };
+              # };
             };
           in
           pkgs.dockerTools.buildLayeredImage {
@@ -51,13 +116,23 @@
             contents = [
               pkgs.coreutils
               pkgs.bash
+              # pkgs.nodejs
+              # pkgs.vips
               cms
             ];
 
             config = {
+              # Env = [
+              # "SHARP_IGNORE_GLOBAL_LIBVIPS=1"
+              # ];
               WorkingDir = "/libexec/cms/deps/cms";
               Cmd = [
-                "${pkgs-legacy.yarn}/bin/yarn"
+                # "${pkgs.nodejs}/bin/npm"
+                # "install"
+                # "--ignore-scripts=false"
+                # "--verbose"
+                # "sharp"
+                "${pkgs.yarn}/bin/yarn"
                 "--offline"
                 "start"
               ];
